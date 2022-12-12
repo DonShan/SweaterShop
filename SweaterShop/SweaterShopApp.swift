4e8c85c3f450ec304df2e53e9c470ae4084359dc
DonShan <msdjshan47@gmail.com>
2022-12-12 07:48:34 +0530

    Initial Commit

---
+++ SweaterShop/SweaterShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweaterShopApp.swift
+//  SweaterShop
+//
+//  Created by Madushan Senavirathna on 2022-12-12.
+//
+
+import SwiftUI
+
+@main
+struct SweaterShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
